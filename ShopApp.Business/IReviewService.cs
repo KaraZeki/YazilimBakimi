be26ab6bddcc62670c7b2fb4382c52bc0ab4deab
OnurYesil <34236403+OnurYesil@users.noreply.github.com>
2020-10-15 19:32:21 +0300

    Business Katmanı
    Varlık sınıflarının işlemlerinin gerçekleştiği iş katmanı  yapıları oluşturuldu.

---
+++ ShopApp.Business/IReviewService.cs
@@ -0,0 +1,12 @@
+﻿using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.Business.Abstract
+{
+   public interface IReviewService
+    {
+        void Create(Review entity);
+    }
+}
