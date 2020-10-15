ac8a2e0bb787369f7b916a985e5c88e47bb36cd6
Capkin <mhmtcpkn85@gmail.com>
2020-10-15 10:57:40 +0300

    DataAccess Katmanı
    DataAccess katmanı için repository pattern yapısı class'lara uygulandı.

---
+++ ShopApp.DataAccess/Concrete/EfCore/EfCoreReviewDal.cs
@@ -0,0 +1,13 @@
+﻿using ShopApp.DataAccess.Abstract;
+using ShopApp.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopApp.DataAccess.Conrete.EfCore
+{
+   public  class EfCoreReviewDal: EfCoreGenericRepository<Review, ShopContext>, IReviewDal
+    {
+
+    }
+}
