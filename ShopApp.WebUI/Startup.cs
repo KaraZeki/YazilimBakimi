0f0656d08d76a7fb5ca3fdfbe45c2ce8b39f526b
mahmutcapkin <mahmutcapkin@hotmail.com>
2020-10-16 23:17:58 +0300

    commit

--- ShopApp.WebUI/Startup.cs
+++ ShopApp.WebUI/Startup.cs
@@ -30,7 +30,7 @@ namespace ShopApp.WebUI
             Configuration = configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)  
         {
@@ -45,13 +45,13 @@ namespace ShopApp.WebUI
             });
 
           
-            //sen e mail services
+            //send e mail services
             services.AddMvc(options => options.EnableEndpointRouting = false);
 
         }
 
         
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) 
         {
             if (env.IsDevelopment())
