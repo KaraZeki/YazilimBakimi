f5540a1f1a44b2da5d26852784fb27c610d8a1c1
mahmutcapkin <mahmutcapkin@hotmail.com>
2020-10-24 19:28:28 +0300

    Mail Service
    Kullanıcı ile ilgili mail işlemleri yapıldı.

---
+++ ShopApp.WebUI/MailServices/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.MailServices
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+    public class EmailSender : IEmailSender
+    {
+        private const string sendGridKey = "sendgrid.com dan apı key alınacak buraya yapıştırılacak";
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(sendGridKey, subject, htmlMessage, email);
+        }
+        private Task Execute(string sendGridKey, string subject, string message, string email)
+        {
+            var client = new SendGridClient(sendGridKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("info@shopapp.com", "Shop App"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+
+            };
+            msg.AddTo(new EmailAddress(email));
+            return client.SendEmailAsync(msg);
+        }
+
+
+    }
+
+}
